3b0664a320e799d4c84c84480dc148a1b5025f36
Eric Watjen <ewatjen@gmail.com>
2011-09-14 16:06:49 -0700

    Added home page and layout

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Loccasions::Application.config.secret_token = '07a772b22f6ac244c904db832bbe5005dbf4088a1679b02dc66bef0cbae32f2c8a64305fdd5d38269f0878956bafeb2dc358cdba7e0a36f75f51d7b238bf6e18'
+Loccasions::Application.config.secret_token = 'ac630b4f4b097a4d42f8fc70528861ce177600500239d53d609db52c38ec02779549ff5a94130d69080152c0ef24f5b738c77d9707126eb0ee67b3aa97647d10'
